bc5430d36b974020d6d4bf283a371668d50c8379
Rob Winch <rwinch@users.noreply.github.com>
2018-07-24 12:11:09 -0700

    saving

---
+++ method-customuser/src/main/java/livelessons/message/MessageService.java
@@ -0,0 +1,8 @@
+package livelessons.message;
+
+import org.springframework.security.access.prepost.PostAuthorize;
+
+public interface MessageService {
+	@PostAuthorize("returnObject?.to?.id == principal?.id")
+	Message getMessage();
+}
---
+++ method/src/main/java/livelessons/MessageService.java
@@ -0,0 +1,5 @@
+package livelessons;
+
+public interface MessageService {
+	String getMessage();
+}
