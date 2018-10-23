58728312b684ce974609da6716d27d0a795d0629
Rob Winch <rwinch@users.noreply.github.com>
2018-10-22 21:39:46 -0500

    Fix AnnotatedMessageServiceTests

--- method-userdetails/src/test/java/livelessons/AnnotatedMessageServiceTests.java
+++ method-userdetails/src/test/java/livelessons/AnnotatedMessageServiceTests.java
@@ -1,16 +1,11 @@
 package livelessons;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.UserDetailsManager;
-import org.springframework.security.test.context.support.TestExecutionEvent;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -35,36 +30,14 @@ public class AnnotatedMessageServiceTests {
 
 	@Test
 	@WithUserDetails
-	public void getMessageWhenAuthorizedThenGranted() {
-		assertThatCode(() -> this.messageService.getMessage()).doesNotThrowAnyException();
+	public void getMessageWhenNotAuthorizedThenGranted() {
+		assertThatCode(() -> this.messageService.getMessage())
+				.isInstanceOf(AccessDeniedException.class);
 	}
 
 	@Test
 	@WithUserDetails("admin")
 	public void getMessageWhenAdminThenGranted() {
-		assertThatCode(() -> this.messageService.getMessage()).doesNotThrowAnyException();
-	}
-
-	@Autowired
-	UserDetailsManager users;
-
-	UserDetails test = User.withUsername("test").password("password").roles("TEST")
-			.build();
-
-	@Before
-	public void setup() {
-		this.users.createUser(this.test);
-	}
-
-	@After
-	public void cleanup() {
-		this.users.deleteUser(this.test.getUsername());
-	}
-
-	@Test
-	// @WithUserDetails("test") // fails
-	@WithUserDetails(value = "test", setupBefore = TestExecutionEvent.TEST_METHOD)
-	public void testUser() {
 		assertThatCode(() -> this.messageService.getMessage()).doesNotThrowAnyException();
 	}
 
